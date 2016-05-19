a614c353c58de068a070aedf32028cd0618f2614
toros11 <tobias.rosen@axsh.net>
2016-05-19 19:39:43 +0900

    Added scripts for gui task to setup parameters

---
+++ notebooks/stepdefs/s-integration-configure-params/exec.sh
@@ -0,0 +1,29 @@
+. /home/centos/notebooks/stepdefs/jenkins-utility/xml-utility.sh
+
+output="$(ssh -qi /home/centos/mykeypair root@10.0.2.100 cat ${job_config} 2> /dev/null | sed 's/&quot;/\"/g')"
+
+param_names=($(get_xml_element_value "parameterDefinitions" <<< "$output" | grep -oP '(?<=<name>).*?(?=</name>)'))
+param_values=($(get_xml_element_value "parameterDefinitions" <<< "$output" | grep -oP '(?<=<defaultValue>).*?(?=</defaultValue>)'))
+
+write_file_path="/var/lib/jenkins/jobs/tiny_web.imagebuild/workspace/jenkins-tiny_web.imagebuild*"
+write_file="$(ssh -qi /home/centos/mykeypair root@10.0.2.100 cat ${write_file_path}  2> /dev/null)"
+
+while read line ; do
+    param_is_set=false
+    value_is_set=false
+
+    saved_param="${line%=*}"
+    saved_value="${line#*=}"
+
+    [[ ${#param_names[@]} -eq ${#param_values[@]} ]] || return 1
+
+    for (( i=0 ; i < ${#param_names[@]} ; i++ )) ; do
+        [[ "$saved_param" == "${param_names[$i]}" ]] && param_is_set=true
+        [[ "$saved_value" == "${param_values[$i]}" ]] && value_is_set=true
+    done
+
+    check_message $param_is_set "$saved_param is used as a build parameter"
+    check_message $value_is_set "$saved_value is used as a value for $saved_param"
+
+done <<< "$write_file"
+
---
+++ notebooks/stepdefs/s-integration-configure-params/pre-exec.sh
@@ -0,0 +1,2 @@
+# Get app image id and db image id from $WRITE_FILE and replace the default value
+# in our xml file
