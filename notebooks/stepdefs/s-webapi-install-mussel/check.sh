c23ed407932ac13e39f155fb190242e83e2d4219
toros11 <tobias.rosen@axsh.net>
2016-05-23 21:51:23 +0900

    Added check message for installing mussel

--- notebooks/stepdefs/s-webapi-install-mussel/check.sh
+++ notebooks/stepdefs/s-webapi-install-mussel/check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+. /home/centos/notebooks/stepdefs/jenkins-utility/message.conf
+. /home/centos/notebooks/stepdefs/jenkins-utility/check_message.sh
 
-ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
-    [[ -f /opt/axsh/wakame-vdc/client/mussel/bin/mussel ]] && echo "Check [ ok]" || echo "Check [ fail ]"
-EOF
+ssh -i /home/centos/mykeypair root@10.0.2.100 "[[ -f /opt/axsh/wakame-vdc/client/mussel/bin/mussel ]] " 2> /dev/null
+
+test_passed=$?
+check_message $test_passed "Mussel installed"
+
+
