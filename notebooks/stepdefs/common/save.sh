d584e2c40b49984134e6e111cd8beae892b9d15f
toros11 <tobias.rosen@axsh.net>
2016-05-24 21:18:30 +0900

    Refactored and cleaned up code

--- notebooks/stepdefs/common/save.sh
+++ notebooks/stepdefs/common/save.sh
@@ -9,41 +9,28 @@ function incremental_log() {
     local filename="${1}"
     let cnt=0
     while [[ -f "$filename-${cnt}" ]] ; do
-	cnt=$(( cnt + 1 ))
+        cnt=$(( cnt + 1 ))
     done
-    cp ${filename} ${filename}-${cnt}
+    cp "${filename}" "${filename}-${cnt}"
 }
 
 function save_config() {
     local file="/var/lib/jenkins/jobs/${job}/config.xml"
-    local xpath="${1}" element_name="${xpath##*/}" base_node="${2}"
+    local xpath="${1}" base_element="${2}" element_name="${xpath##*/}"
 
     ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF 2> /dev/null
         $(declare -f xml_save_backup)
         xml_save_backup "${file}" "${element_name}" "${xpath}"
 EOF
-    if [[ ! -z $base_node ]] ; then
-        scp -i /home/centos/mykeypair root@10.0.2.100:/tmp/"${element_name}".data-student $(dirname $0)/xml-data/"${2}".data-student &> /dev/null
-        incremental_log $(dirname $0)/xml-data/"${2}".data-student
-    else
-        scp -i /home/centos/mykeypair root@10.0.2.100:/tmp/"${element_name}".data-student $(dirname $0)/xml-data/ &> /dev/null
-        incremental_log $(dirname $0)/xml-data/"${element_name}".data-student
-    fi
+    scp -i /home/centos/mykeypair root@10.0.2.100:/tmp/"${element_name}".data-student $(dirname $0)/xml-data/"${base_element}".data-student &> /dev/null
+    incremental_log $(dirname $0)/xml-data/"${base_element}".data-student
 }
 
 echo "Saving progress..."
-[[ ${#xpaths} -eq 0 ]] || {
-    for xpath in "${xpaths[@]}" ; do
-        [[ ${#insert_to} -eq 0 ]] || {
-            unset base_node
-            contains_value "${xpath}" "${insert_to[@]}" && {
-                base_node="${xpath##*/}"
-                xpath="${element}"
-            }
-        }
-        save_config "${xpath}" "${base_node}"
-    done
-}
+for xpath in "${xpaths[@]}" ; do
+    base_element="${xpath##*/}"
+    [[ ${#insert_to} -eq 0 ]] || { contains_value "${xpath}" "${insert_to[@]}" && xpath="${xpath}/${element}" ; }
 
-
+    save_config "${xpath}" "${base_element}"
+done
 
