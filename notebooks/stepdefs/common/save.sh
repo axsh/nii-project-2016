0d469fbf803b97e943e0c00e001df28ed24d8c15
toros11 <tobias.rosen@axsh.net>
2016-05-24 18:16:37 +0900

    Fixes a bug where configurations that gets inserted does not get saved properly

--- notebooks/stepdefs/common/save.sh
+++ notebooks/stepdefs/common/save.sh
@@ -7,19 +7,32 @@
 
 function save_config() {
     local file="/var/lib/jenkins/jobs/${job}/config.xml"
-    local xpath="${1}" element_name="${xpath##*/}"
+    local xpath="${1}" element_name="${xpath##*/}" base_node="${2}"
 
     ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF 2> /dev/null
         $(declare -f xml_save_backup)
         xml_save_backup "${file}" "${element_name}" "${xpath}"
 EOF
-    scp -i /home/centos/mykeypair root@10.0.2.100:/tmp/"${element_name}".data-student $(dirname $0)/xml-data/ &> /dev/null
+    if [[ ! -z $base_node ]] ; then
+        scp -i /home/centos/mykeypair root@10.0.2.100:/tmp/"${element_name}".data-student $(dirname $0)/xml-data/"${2}".data-student &> /dev/null
+    else
+        scp -i /home/centos/mykeypair root@10.0.2.100:/tmp/"${element_name}".data-student $(dirname $0)/xml-data/ &> /dev/null
+    fi
 }
 
 echo "Saving progress..."
 [[ ${#xpaths} -eq 0 ]] || {
     for xpath in "${xpaths[@]}" ; do
-        [[ ${#insert_to} -eq 0 ]] || { contains_value "${xpath}" "${insert_to[@]}" && xpath="${element}" ; }
-        save_config "${xpath}"
+        [[ ${#insert_to} -eq 0 ]] || {
+            unset base_node
+            contains_value "${xpath}" "${insert_to[@]}" && {
+                base_node="${xpath##*/}"
+                xpath="${element}"
+            }
+        }
+        save_config "${xpath}" "${base_node}"
     done
 }
+
+
+
