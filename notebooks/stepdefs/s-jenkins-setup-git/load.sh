cb741c652d0a26fc22422e609352dfe148ebc2c3
Tobias Rosen <tobias.rosen@axsh.net>
2017-05-18 04:39:21 +0900

    Fix scp to work with port number

--- notebooks/stepdefs/s-jenkins-setup-git/load.sh
+++ notebooks/stepdefs/s-jenkins-setup-git/load.sh
@@ -3,4 +3,4 @@
 . $(dirname $0)/stepdata.conf
 
 [[ -f $(dirname $0)/xml-data/"${filename##*/}" ]] &&
-    scp -i ~/mykeypair $(dirname $0)/xml-data/"${filename##*/}"  root@${INSTANCE_IP} -p ${INSTANCE_PORT}:"$filename"
+    scp -i ~/mykeypair -P ${INSTANCE_PORT} $(dirname $0)/xml-data/"${filename##*/}" root@${INSTANCE_IP}:"$filename"
