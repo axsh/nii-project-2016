3d04a179af1dd525546afd360755635dc7959e7a
Tobias Rosen <tobias.rosen@axsh.net>
2017-05-19 21:01:14 +0900

    Added missing variables for ip/port

--- notebooks/stepdefs/s-jenkins-setup-git/load.sh
+++ notebooks/stepdefs/s-jenkins-setup-git/load.sh
@@ -2,5 +2,8 @@
 
 . $(dirname $0)/stepdata.conf
 
+INSTANCE_IP=$(< ~/vdc_host_ip)
+INSTANCE_PORT=$(< ~/vdc_instance_port)
+
 [[ -f $(dirname $0)/xml-data/"${filename##*/}" ]] &&
     scp -i ~/mykeypair -P ${INSTANCE_PORT} $(dirname $0)/xml-data/"${filename##*/}" root@${INSTANCE_IP}:"$filename"
--- notebooks/stepdefs/s-jenkins-setup-hipchat/save.sh
+++ notebooks/stepdefs/s-jenkins-setup-hipchat/save.sh
@@ -2,4 +2,7 @@
 
 . $(dirname $0)/stepdata.conf
 
+INSTANCE_IP=$(< ~/vdc_host_ip)
+INSTANCE_PORT=$(< ~/vdc_instance_port)
+
 scp -i ~/mykeypair -P ${INSTANCE_PORT} root@${INSTANCE_IP}:"$filename" $(dirname $0)/xml-data/ &> /dev/null
